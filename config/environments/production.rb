970f5196021e74b69991cacfabee4993eddaf3cf
Rick Bacci <rickbacci@gmail.com>
2015-11-02 22:11:03 -0700

    fix deprecation error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
 
 
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
 
