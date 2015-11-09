44f9fe067c7146b0836c942416ae96bfa442543d
Rick Bacci <rickbacci@gmail.com>
2015-11-09 01:30:40 -0700

    clean up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,32 @@
 Rails.application.routes.draw do
-
   root "home#index"
 
-  get "/browse", to: "loan_requests#index"
-
+  get "/browse",    to: "loan_requests#index"
   get "/portfolio", to: "borrower_portfolio#show"
 
   resources :payment, only: [:update]
 
   resources :loan_requests
 
-  get "/cart", to: "cart#index"
-  post "/cart", to: "cart#create"
+  get "/cart",    to: "cart#index"
+  post "/cart",   to: "cart#create"
   delete "/cart", to: "cart#delete"
-  put "/cart", to: "cart#update"
+  put "/cart",    to: "cart#update"
 
   resources :orders, only: [:create, :index, :show, :update]
 
-  get "/login", to: "sessions#new", :as => "login"
-  post "/login", to: "sessions#create"
-  get "/logout", to: "sessions#destroy"
+  get "/login",     to: "sessions#new", :as => "login"
+  post "/login",    to: "sessions#create"
+  get "/logout",    to: "sessions#destroy"
   delete "/logout", to: "sessions#destroy"
 
   resources :lenders
 
   resources :borrowers
 
-  resources :users, only: [:show]
+  resources :users,      only: [:show]
   resources :categories, only: [:show]
 
-  get "*path", to: "home#not_found"
+  get "/404" => "errors#not_found"
+  get "/500" => "errors#internal_server_error"
 end
